6c6ab89adb9016475f7e58713baaf131a2449f68
Senaya-KD <senaya101@outlook.com>
2026-01-19 15:09:14 +0530

    adding data loading logger in main

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main():
 
     # Step 1: Preprocess data
     df = preprocess_run()  # MUST return a DataFrame
-    logger.info("Data preprocessing completed")
+    logger.info("Data testing and Data preprocessing completed")
 
     # Step 2: Feature engineering
     df = add_features(df)  # MUST return df with new features
